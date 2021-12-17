2fb36c294e208508172dabea7aeb3ad3053012d9
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-12-17 23:38:48 +0100

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
 import './App.css';
-import Feedback from './components/Feedback/Feedback';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Container from './components/Container/Container';
+import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Notification from './components/Notification/Notification';
+import Statistics from './components/Statistics/Statistics';
 
-function App() {
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = option => {
+    option === 'good' && setGood(prevState => prevState + 1);
+    option === 'neutral' && setNeutral(prevState => prevState + 1);
+    option === 'bad' && setBad(prevState => prevState + 1);
+  };
+
+  const feedbackOptions = { good, neutral, bad };
+
+  const countTotalFeedback = () =>
+    Object.values(feedbackOptions).reduce((acc, curr) => acc + curr);
+
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good / countTotalFeedback()) * 100);
+
+  const getStatisticsOptions = () => [
+    ...Object.entries(feedbackOptions),
+    ['total', countTotalFeedback()],
+    ['positive feedback', countPositiveFeedbackPercentage() + `%`],
+  ];
+
   return (
     <main className="App">
-      <Feedback className="Feedback" />
+      <Container>
+        <Section title="Please leave Feedback">
+          <FeedbackOptions
+            options={Object.keys(feedbackOptions)}
+            onLeaveFeedback={handleFeedback}
+          />
+        </Section>
+
+        {countTotalFeedback() === 0 &&
+        isNaN(countPositiveFeedbackPercentage()) ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics options={getStatisticsOptions()} />
+          </Section>
+        )}
+      </Container>
     </main>
   );
 }
 
-export default App;
+App.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from '../Statistics/Statistics.module.css';
+import styles from './Statistics.module.css';
 
 const Statistics = ({ options }) => {
   return (
     <ul className={styles.List}>
-      {options.map((item, idx) => (
+      {options.map(([label, value], idx) => (
         <li key={idx} className={styles.Item}>
-          <p className={styles.Label}>{item[0]}:</p>
-          <p className={styles.Amount}>{item[1]}</p>
+          <p className={styles.Label}>{label}:</p>
+          <p className={styles.Amount}>{value}</p>
         </li>
       ))}
     </ul>
